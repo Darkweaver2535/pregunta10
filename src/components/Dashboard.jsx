d17c62472378515b09f5ca1ca8f9bf97870b22cf
Darkweaver <albasantiagoe@hotmail.com>
2023-11-30 12:00:56 -0400

    componente 2 realizado

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "Dashboard.css";
+
+function Dashboard() {
+  return <div className="about"></div>;
+}
+export default Dashboard;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "Header.css";
+
+function Header() {
+  return <div className="about"></div>;
+}
+export default Header;
